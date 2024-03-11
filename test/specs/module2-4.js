24852ea1aec0feb7ec8894b5c8b7014bd7e54472
thanh ha <thanhhanguyen6103@gmail.com>
2024-03-12 00:59:15 +0700

    module 2-1 -> 2-8

---
+++ test/specs/module2-4.js
@@ -0,0 +1,111 @@
+describe('Module 2-4: Thêm/Xóa/Sửa note của một tài liệu', () => {
+    before(()=>{
+        module1_5();
+    })
+
+    after(()=>{
+        browser.closeWindow();
+    })
+
+    //Thêm/Sửa/Xóa note cho tài liệu 
+    let fileRevisionActiveURL = process.env.URL_QLTLQT;
+    let btnEdit = '[title="Sửa quy trình"]';
+    let btnSave = '[class="btn btn-primary"]';
+    let inputNote = '[name="note"]'; 
+
+    it('Thêm / Sửa note cho tài liệu thành công', () =>{
+
+        browser.url(fileRevisionActiveURL);
+        $(btnEdit).click();
+
+        $(inputNote).waitForDisplayed(5000);
+        $(inputNote).setValue('note cho tài liệu 1');
+
+        $(btnSave).waitForDisplayed(3000);
+        $(btnSave).click();
+        browser.pause(3000);
+
+        //Expect output: Hiển thị thông báo sửa thành công 
+        let toast = '[class="Toastify__toast Toastify__toast-theme--light Toastify__toast--success"]';
+        expect(toast).toBeDisplayed();
+    })
+
+    it('Xóa note cho tài liệu thành công', () =>{
+
+        browser.url(fileRevisionActiveURL);
+
+        $(inputNote).waitForDisplayed(5000);
+        $(inputNote).clearValue();
+        browser.pause(2000);
+        $(btnEdit).click();
+
+        $(btnSave).waitForDisplayed(3000);
+        $(btnSave).click();
+        browser.pause(3000);
+
+        //Expect output: chắc chắn hiển thị thông báo sửa thành công 
+        let toast = '[class="Toastify__toast Toastify__toast-theme--light Toastify__toast--success"]';
+        expect(toast).toBeDisplayed(true);
+    })
+
+});
+
+function module1_5(){
+    let pageurl = process.env.URL_LOGIN;
+    let Loginbtn = '[class="btn btn-primary"]';
+    let inputusename = '[type="text"]';
+    let inputpass = '[type="password"]';
+    let Quytrinh = 'Quy trình';
+    it('điều hướng đến trang đăng nhập và đăng nhập thành công với tư cách người dùng quản trị viên', () => {
+
+        //đăng nhập vào page
+        browser.url(pageurl);
+
+        $(inputusename).waitForDisplayed(5000);
+        //input username
+        $(inputusename).setValue('admin0');
+        $(inputpass).setValue('matkhauadmin123');
+        browser.pause(2000);
+        $(Loginbtn).click();
+    });
+
+    it('Điều hướng đến "Quản lý thư mục"', () =>{
+
+        //điều hướng đến trang quản lý thư mục
+        browser.url(Linkpage);
+        browser.pause(5000);
+
+    });
+
+    let Linkpage = process.env.URL_QLTM;
+    let Botton = '[class="btn btn-primary"]';
+
+    it('- Trình duyệt được điều hướng đến trang "Quản lý thưc mục" ',() => {
+
+       // Nhấp vào ô "Quy trình" của bất kỳ thư mục nào
+        $(Botton).click();
+
+        browser.pause(5000)
+    })
+    
+    let LinkPage1 = process.env.URL_QLQTTM;
+    let OptiomClick = '[data-id="HT - QT - 02"]'
+    it('Điều hướng đến "Quản lý phiên bản hiệu lực"', () =>{
+
+        browser.url(LinkPage1);
+
+        $(OptiomClick).waitForDisplayed(5000);
+        $(OptiomClick).selectByVisibleText('Tài liệu hiệu lực');
+        browser.pause(3000);
+    })
+
+    let revisionActiveURL = process.env.URL_QLPBHL;
+    it('Điều hướng đến "Quản lý tài liệu của quy trình "', () =>{
+
+        browser.url(revisionActiveURL);
+
+        $(Botton).waitForDisplayed(5000);
+        $(Botton).selectByVisibleText('Tài liệu');
+        browser.pause(3000);
+    })
+}
